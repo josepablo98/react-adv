44dd6f6bb0ec4bdee8c1f7b6edb0b97449417587
Jose Pablo <josepabloruizperez99@gmail.com>
2023-11-26 13:54:14 +0100

    Main actualizado

--- src/routes/Navigation.tsx
+++ src/routes/Navigation.tsx
@@ -1,6 +1,5 @@
 import { BrowserRouter, Routes, Route, NavLink, Navigate } from "react-router-dom"
 import logo from '../logo.svg'
-import { ShoppingPage } from "../02-component-patterns/pages/ShoppingPage"
 export const Navigation = () => {
   return (
     <BrowserRouter>
@@ -25,7 +24,7 @@ export const Navigation = () => {
             <Routes>
                 <Route path="about" element={<h1>About Page</h1>} />
                 <Route path="users" element={<h1>Users Page</h1>} />
-                <Route path="shopping" element={<ShoppingPage />} />
+                <Route path="shopping" element={<h1>Shopping Page</h1>} />
                 <Route path="/*" element={<Navigate to="/shopping" replace />} />
             </Routes>
 
